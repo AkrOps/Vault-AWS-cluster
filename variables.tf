6e45ad82e24243a4b635e7782e72dafee46a80e2
JBeCast <jbermudez@thecloudsaviour.com>
2022-03-17 14:54:20 +0100

    misc: add var types

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "aws_region" {
+  type    = string
   default = "eu-central-1"
 }
 
 variable "aws_zone" {
+  type    = string
   default = "eu-central-1a"
 }
 
 variable "vault_url" {
+  type    = string
   default = "https://releases.hashicorp.com/vault/1.9.3/vault_1.9.3_linux_amd64.zip"
 }
 
