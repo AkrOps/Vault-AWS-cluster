1ee760ddb3886b7c69ca74c8696d5895b06f836b
JBeCast <jbermudez@thecloudsaviour.com>
2022-04-30 13:32:41 +0200

    cleanup: remove previous userdata tpl and vault_url var

--- variables.tf
+++ variables.tf
@@ -14,11 +14,6 @@ variable "name_prefix" {
   description = "Prefix for tagging and naming AWS resources"
 }
 
-variable "vault_url" {
-  type    = string
-  default = "https://releases.hashicorp.com/vault/1.9.3/vault_1.9.3_linux_amd64.zip"
-}
-
 variable "vpc_id" {
   type        = string
   description = "The ID of the VPC id where the Vault cluster should be provisioned."
